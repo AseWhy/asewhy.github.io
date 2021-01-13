3049a6ab105a2b70242c8070fb2d2029900d7586
Astecom <astecom@mail.ru>
2021-01-14 01:08:21 +0400

    ccs fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import App from './App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { Theming, PageManager } from './data/scripts/main'
 
 window.PageManager = PageManager;
@@ -21,4 +19,6 @@ new Vue({
   },
 
   template: '<App/>'
-})
+});
+
+import 'bootstrap/dist/css/bootstrap.min.css';
