80579dc7cbf783e1f839611ccaf7a1de1c2b40e1
Astecom <astecom@mail.ru>
2021-01-18 21:44:16 +0400

    xml and html highlight fix

--- src/data/scripts/modules/page-manager.js
+++ src/data/scripts/modules/page-manager.js
@@ -4,8 +4,11 @@ import marked from 'marked';
 
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
+import xml from 'highlight.js/lib/languages/xml';
 
 hljs.registerLanguage('js', javascript);
+hljs.registerLanguage('html', xml);
+hljs.registerLanguage('xml', xml);
 
 marked.setOptions({
     highlight: (code, language) => {
