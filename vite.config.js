081725939231465ca08feac5e524056b09bb19a8
plekhanov <plekhanov@bifit.com>
2024-01-11 17:36:31 +0400

    Update texts

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer as viteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [ react() ],
+    plugins: [
+        react(),
+        viteImageOptimizer({
+            png: {
+                quality: 90
+            },
+            jpeg: {
+                quality: 90
+            },
+            jpg: {
+                quality: 90
+            }
+        })
+    ],
     server: {
         host: true
     }
